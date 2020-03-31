db8a6d4860c206bed167e4101e0b199caa28502f
SJalltheway5 <SJalltheway5@aol.com>
2020-03-31 02:13:20 -0700

    Initial commit

---
+++ example_run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Wine application script template
+# vvv  Remove this line  vvv
+echo "Edit this script before running."; exit 1
+
+# Change to script directory
+cd "${0%/*}"
+
+# Set up Wine environment
+source "./example_env"
+
+# Set application .exe to run
+EXE="example.exe"
+
+# Run application
+cd "."
+wine "${EXE}" "${@}"

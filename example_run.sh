a35037aa153e4983ce5ed01c3d6c168a51566639
Seth Johnson <sjalltheway5@aol.com>
2020-10-16 22:33:36 -0700

    conform to shellcheck

--- example_run.sh
+++ example_run.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-# Wine application script template
+# Wine application example launch script
 # vvv  Remove this line  vvv
-echo "Edit this script before running."; exit 1
+echo "Edit this script before running."; exit
 
-# Change to script directory
-cd "${0%/*}" || exit 1
+# Change to script's directory
+cd "$(cd "${0%/*}" && pwd)" || exit 1
 
 # Set up Wine environment
 source "./example_env"
 
 # Set application .exe to run
-EXE="example.exe"
+EXE="bin/example.exe"
 
 # Run application
-cd "." || exit 1
+cd "./bin" || exit 1
 wine "${EXE}" "${@}"
